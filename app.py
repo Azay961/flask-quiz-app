e06103419ba1805dac9cdc5d3481ec2c56f2581c
Azay961 <ajay023.chaudhary@gmail.com>
2024-04-27 08:57:03 +0545

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
+
 from flask import Flask, render_template, request
+import pandas as pd
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+
+@app.route('/')
+def index():
+    return render_template('questions.html')
+
+@app.route('/take_quiz', methods=['POST'])
+def predict_sentiment():
+    input_text = request.form['input_text']
+    subject = request.form['model_dropdown']
+    if subject == "Computer Network":
+        df = pd.read_csv(r'D:\my_projects\Flask_quiz_app\flask-quiz-app\Questions\computer_network.csv')
+    
     return render_template('index.html')
 
-
-@app.route('/computer_organization', methods=['GET', 'POST'])
-def search():
-    if request.method == 'POST':
-        query = request.form.get("input_text")
-
-   
-        return render_template('index.html', query=query, results=query)
-
-        
+@app.route("/add_questions", methods=["POST"])
+def add_questions():
+    subject = request.form['model_dropdown']
+    render_template("questions.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # host="0.0.0.0", port="5002"
