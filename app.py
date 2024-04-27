cbc6299b95240cb3540f927770ac6cf3787cb685
Azay961 <ajay023.chaudhary@gmail.com>
2024-04-27 12:54:47 +0545

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 
 app = Flask(__name__)
@@ -9,15 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('questions.html')
-
+# taking quiz
 @app.route('/take_quiz', methods=['POST'])
 def predict_sentiment():
-    input_text = request.form['input_text']
-    subject = request.form['model_dropdown']
-    if subject == "Computer Network":
-        df = pd.read_csv(r'D:\my_projects\Flask_quiz_app\flask-quiz-app\Questions\computer_network.csv')
+    subject = request.form['select_subject']
+    file_path = os.path.join("Questions", f"{subject}.csv")
+    # Load a DataFrame with randomly selected 5 rows
+    random_df = pd.read_csv(file_path).sample(n=5)
     
-    return render_template('index.html')
+    return render_template('index.html', mcq=random_df)
 
 # Adding question to file
 @app.route("/add_questions", methods=["POST"])
@@ -31,14 +32,25 @@ def add_questions():
     correct_answer = request.form["correct_answer"]
 
     # Create a DataFrame
-    data = {'Questions': question, 'option1': option1, 'option2': option2, 'option3': option3, "option4": option4, "correct_answer": correct_answer}
+    data = {'Questions': question, 'option1':[option1], 'option2': [option2], 'option3': [option3], "option4": [option4], "correct_answer": [correct_answer]}
     df = pd.DataFrame(data)
 
-    # Save DataFrame to CSV
-    file_path = f"/Questions/{subject}.csv"
-    df.to_csv(file_path, mode='a', index=index, header=False)
+    # Specify the directory path
+    directory = "Questions"
 
-    render_template("questions.html")
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Specify the file path
+    file_path = os.path.join(directory, f"{subject}.csv")
+
+    file_exists = os.path.exists(file_path)
+
+    # Write DataFrame to CSV
+    df.to_csv(file_path, index=False, mode='a', header=not file_exists)
+
+    return render_template("questions.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
