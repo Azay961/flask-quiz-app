aaf4a7d8cd1133ea51a5021ad76199ecb1fe2e30
Azay961 <ajay023.chaudhary@gmail.com>
2024-04-27 10:26:42 +0545

    Update

--- app.py
+++ app.py
@@ -19,9 +19,25 @@ def predict_sentiment():
     
     return render_template('index.html')
 
+# Adding question to file
 @app.route("/add_questions", methods=["POST"])
 def add_questions():
-    subject = request.form['model_dropdown']
+    subject = request.form['select_subject']
+    question = request.form["question"]
+    option1 = request.form["option1"]
+    option2 = request.form["option2"]
+    option3 = request.form["option3"]
+    option4 = request.form["option4"]
+    correct_answer = request.form["correct_answer"]
+
+    # Create a DataFrame
+    data = {'Questions': question, 'option1': option1, 'option2': option2, 'option3': option3, "option4": option4, "correct_answer": correct_answer}
+    df = pd.DataFrame(data)
+
+    # Save DataFrame to CSV
+    file_path = f"/Questions/{subject}.csv"
+    df.to_csv(file_path, mode='a', index=index, header=False)
+
     render_template("questions.html")
 
 if __name__ == "__main__":
